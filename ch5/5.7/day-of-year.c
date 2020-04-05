2db32f9a738116c557715e490a36e354036d796b
Samuel Tang <samuel.x.tang@gmail.com>
2020-04-05 22:15:17 +0800

    exercise5_9 replace indexing with pointers

--- ch5/5.7/day-of-year.c
+++ ch5/5.7/day-of-year.c
@@ -1,31 +1,31 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-static char daytab[2][13] = {
+static int daytab[][13] = {
     {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31},
     {0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31}
 };
 
 /* day_of_year: set day of year from month & day */
 int day_of_year(int year, int month, int day) {
-    int leap, i;
+    int leap, *pm;
 
     leap = (year % 4 == 0 && year % 100) || year % 400 == 0;
-    for (i = 1; i < month; i++)
-        day += daytab[leap][i];
+    for (pm = *(daytab+leap); pm - *(daytab+leap) < month; pm++)
+        day += *pm;
     return day;
 }
 
 
 /* month_day: */
 void month_day(int year, int yearday, int *pmonth, int *pday) {
-    int leap, i;
+    int leap, *pm;
 
     leap = (year % 4 == 0 && year % 100) || year % 400 == 0;
-    for (i = 1; yearday > daytab[leap][i]; i++)
-        yearday -= daytab[leap][i];
+    for (pm = *(daytab+leap); yearday > *pm; pm++)
+        yearday -= *pm;
 
-    *pmonth = i;
+    *pmonth = pm - *(daytab+leap);
     *pday = yearday;
 }
 
